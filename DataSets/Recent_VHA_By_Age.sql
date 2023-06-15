6cb2e7d36c6e29f17ba31bc4e1c6737eb30e74ab
Adam Klima <adamrklima@gmail.com>
2023-06-15 17:46:04 -0400

    Python Editing and Comments

--- DataSets/Recent_VHA_By_Age.sql
+++ DataSets/Recent_VHA_By_Age.sql
@@ -1,8 +1,57 @@
-
 SELECT *
 FROM NumofSuicByState
-WHERE Year < 2015
+
+DELETE FROM AgeByState
+WHERE Year<2015
+
+SELECT * FROM AgeByState
+
+DELETE FROM HispanicAffiliationRates
+WHERE Year<2015
+
+SELECT * FROM HispanicAffiliationRates
 
 DELETE FROM NumofSuicByState
 WHERE Year<2015
 
+SELECT * FROM NumofSuicByState
+
+DELETE FROM RaceByState
+WHERE Year<2015
+
+SELECT * FROM RaceByState
+
+DELETE FROM RatesBySex
+WHERE Year<2015
+
+SELECT * FROM RatesBySex
+
+DELETE FROM RatesByRace
+WHERE Year<2015
+
+SELECT * FROM RatesByRace
+
+DELETE FROM RatesBySexandAge
+WHERE Year<2015
+
+SELECT * FROM RatesBySexandAge
+
+DELETE FROM RecentVHAbyAge
+WHERE Year<2015
+
+SELECT * FROM RecentVHAbyAge
+
+DELETE FROM SexByYearAndState
+WHERE Year<2015
+
+SELECT * FROM SexByYearAndState
+
+DELETE FROM NonRecentVHAbyYear
+WHERE Year<2015
+
+SELECT * FROM NonRecentVHAbyYear
+
+DELETE FROM NonRecentVHAbyAge
+WHERE Year<2015
+
+SELECT * FROM NonRecentVHAbyAge
