1d4d7fdc8bbe97b7ed08f3928bbb291a6680c1ad
Adam Klima <adamrklima@gmail.com>
2023-06-19 18:45:31 -0400

    Database Scrubbing

--- DataSets/Recent_VHA_By_Age.sql
+++ DataSets/Recent_VHA_By_Age.sql
@@ -60,3 +60,76 @@ SELECT rr.*, rs.*
 FROM RatesByRace rr
 LEFT JOIN RatesBySex rs
 on rr.Year=rs.Year
+
+SELECT rtrim("SuicideRate/100,000", '*') as trimmed
+FROM AgeByState
+
+UPDATE AgeByState
+SET "SuicideRate/100,000"=rtrim("SuicideRate/100,000", '*')
+
+SELECT "SuicideRate/100,000"
+FROM AgeByState
+
+CREATE TABLE RecentVHAbyYear as
+	SELECT *
+	FROM RecentVHAbyAge
+	WHERE AgeGroup='All'
+
+CREATE TABLE RecentAndNonRecentVHAbyYear as
+	SELECT nry.*, ry.*
+	FROM NonRecentVHAbyYear nry
+	LEFT JOIN RecentVHAbyYear ry
+	on nry.Year=ry.Year
+
+ALTER TABLE RecentAndNonRecentVHAbyYear
+DROP COLUMN NonRecentVHAMaleSuicides
+
+ALTER TABLE RecentAndNonRecentVHAbyYear
+DROP COLUMN MaleNonRecentVHApopEst
+
+ALTER TABLE RecentAndNonRecentVHAbyYear
+DROP COLUMN "MaleNonRecentVHAuserCrudeRate/100,000"
+
+ALTER TABLE RecentAndNonRecentVHAbyYear
+DROP COLUMN FemaleNonRecentVHAuserSuicides
+
+ALTER TABLE RecentAndNonRecentVHAbyYear
+DROP COLUMN FemaleNonRecentVHAuserPopEst
+
+ALTER TABLE RecentAndNonRecentVHAbyYear
+DROP COLUMN "FemaleNonRecentVHAuserCrudeRate/100,000"
+
+ALTER TABLE RecentAndNonRecentVHAbyYear
+DROP COLUMN "Year:1"
+
+ALTER TABLE RecentAndNonRecentVHAbyYear
+DROP COLUMN AgeGroup
+
+ALTER TABLE HispanicAffiliationRates
+DROP COLUMN "AgeAdjustedRate/100,000"
+
+ALTER TABLE HispanicAffiliationRates
+DROP COLUMN "AgeAndSexAdjustedRate/100,000"
+
+ALTER TABLE HispanicAffiliationRates
+DROP COLUMN "MaleAgeAdjustedRate/100,000"
+
+ALTER TABLE HispanicAffiliationRates
+DROP COLUMN "FemaleAgeAdjustedRate/100,000"
+
+ALTER TABLE RecentVHAbyYear
+DROP COLUMN AgeGroup
+
+
+
+ALTER TABLE RatesBySexAndRace
+DROP COLUMN NonRecentVHAMaleSuicides
+
+ALTER TABLE RecentAndNonRecentVHAbyYear
+DROP COLUMN MaleNonRecentVHApopEst
+
+ALTER TABLE RecentAndNonRecentVHAbyYear
+DROP COLUMN "MaleNonRecentVHAuserCrudeRate/100,000"
+
+
+
