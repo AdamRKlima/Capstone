d3c300bacf6e2b05a59ce3c8a8380c25ac624725
Adam Klima <adamrklima@gmail.com>
2023-06-27 13:05:20 -0400

    Capstone Updates

--- DataSets/Recent_VHA_By_Age.sql
+++ DataSets/Recent_VHA_By_Age.sql
@@ -176,3 +176,301 @@ ADD COLUMN Addd as (Suicides+"SuicideRate/100,000")
 
 ALTER TABLE AgeByState
 DROP COLUMN Addd
+
+CREATE TABLE SuicideByYear as
+	SELECT *
+	FROM StatebyYear
+	WHERE GeoRegion='All'
+	
+ALTER TABLE SuicideByYear
+DROP COLUMN GeoRegion
+
+ALTER TABLE SuicideByYear
+DROP COLUMN State
+
+ALTER TABLE RecentAndNonRecentVHAbyYear
+ADD COLUMN NumofSuicide
+
+UPDATE RecentAndNonRecentVHAbyYear
+	SET NumofSuicide =(
+		SELECT NumofSuicide
+		FROM SuicideByYear
+		WHERE RecentAndNonRecentVHAbyYear.Year = SuicideByYear.Year)
+
+ALTER TABLE RecentAndNonRecentVHAbyYear
+DROP COLUMN RecentVHApercentage
+
+ALTER TABLE RecentAndNonRecentVHAbyYear
+ADD COLUMN RecentVHApercentage 
+
+
+UPDATE RecentAndNonRecentVHAbyYear
+SET NonRecentVHAsuicides=replace(NonRecentVHAsuicides, ',','')
+
+SELECT NonRecentVHAsuicides
+FROM RecentAndNonRecentVHAbyYear
+
+UPDATE RecentAndNonRecentVHAbyYear
+SET NonRecentVHApop=replace(NonRecentVHApop, ',','')
+
+
+UPDATE RecentAndNonRecentVHAbyYear
+SET NumOfRecentVHAuserSuicide=replace(NumOfRecentVHAuserSuicide, ',','')
+
+UPDATE RecentAndNonRecentVHAbyYear
+SET VHAvetPopEst=replace(VHAvetPopEst, ',','')
+
+UPDATE RecentAndNonRecentVHAbyYear
+	SET RecentVHApercentage =(
+		SELECT round((100*(NumOfRecentVHAuserSuicide/NumofSuicide)),2)
+		FROM RecentAndNonRecentVHAbyYear)
+
+SELECT RecentVHApercentage
+FROM RecentAndNonRecentVHAbyYear
+
+ALTER TABLE RecentAndNonRecentVHAbyYear
+ADD COLUMN NonRecentVHApercentage 
+
+UPDATE RecentAndNonRecentVHAbyYear
+	SET NonRecentVHApercentage=(
+		SELECT round((100*(NonRecentVHAsuicides/NumofSuicide)),2)
+		FROM RecentAndNonRecentVHAbyYear)
+		
+SELECT NonRecentVHApercentage
+FROM RecentAndNonRecentVHAbyYear
+
+-- SexByYearAndState RecentAndNonRecentVHAbyYear RatesBySexAndRace
+CREATE TABLE YearOverview as
+	SELECT ss.*, rby.*, rsr.*
+	FROM SuicideByYear ss
+	NATURAL LEFT JOIN RecentAndNonRecentVHAbyYear rby
+	NATURAL LEFT JOIN RatesBySexAndRace rsr
+
+UPDATE YearOverview
+SET WhiteNumofSuicide=replace(WhiteNumofSuicide, ',','')
+
+UPDATE YearOverview
+SET SuicideDeaths=replace(SuicideDeaths, ',','')
+
+UPDATE YearOverview
+SET MalePopEst=replace(MalePopEst, ',','')
+
+UPDATE YearOverview
+SET FemalePopEst=replace(FemalePopEst, ',','')
+
+ALTER TABLE YearOverview
+DROP COLUMN "Year:1"
+
+ALTER TABLE YearOverview
+DROP COLUMN "Year:2"
+
+ALTER TABLE YearOverview
+DROP COLUMN "NumofSuicide:1"
+
+ALTER TABLE YearOverview
+DROP COLUMN "Percent"
+
+ALTER TABLE YearOverview
+DROP COLUMN "AgeAdjustedRate/100,000"
+
+ALTER TABLE YearOverview
+DROP COLUMN "AgeAndSexAdjustedRate/100,000"
+
+ALTER TABLE YearOverview
+DROP COLUMN "MaleAgeAdjustedRate/100,000"
+
+ALTER TABLE YearOverview
+DROP COLUMN "FemaleAgeAdjustedRate/100,000"
+
+UPDATE AgeOverview
+SET NumofSuicide=replace(NumofSuicide, ',','')
+
+UPDATE AgeOverview
+SET VeteranPopEst=replace(VeteranPopEst, ',','')
+
+UPDATE AgeOverview
+SET MaleSuicide=replace(MaleSuicide, ',','')
+
+UPDATE AgeOverview
+SET MalePopEst=replace(MalePopEst, ',','')
+
+UPDATE AgeOverview
+SET FemaleNumofSuicide=replace(FemaleNumofSuicide, ',','')
+
+UPDATE AgeOverview
+SET FemalePopEst=replace(FemalePopEst, ',','')
+
+UPDATE AgeOverview
+SET NonRecentVHAsuicides=replace(NonRecentVHAsuicides, ',','')
+
+UPDATE AgeOverview
+SET NonRecentVHAuserPopEst=replace(NonRecentVHAuserPopEst, ',','')
+
+UPDATE AgeOverview
+SET NumOfRecentVHAuserSuicide=replace(NumOfRecentVHAuserSuicide, ',','')
+
+UPDATE AgeOverview
+SET VHAvetPopEst=replace(VHAvetPopEst, ',','')
+
+ALTER TABLE AgeOverview
+ADD COLUMN MaleSuicidePercent
+
+UPDATE AgeOverview
+	SET MaleSuicidePercent=(
+		SELECT round((100*(MaleSuicide/NumofSuicide)),2)
+		FROM AgeOverview)
+		
+ALTER TABLE AgeOverview
+ADD COLUMN MalePopPercent
+
+UPDATE AgeOverview
+	SET MalePopPercent=(
+		SELECT round((100*(MalePopEst/VeteranPopEst)),2)
+		FROM AgeOverview)
+
+ALTER TABLE AgeOverview
+ADD COLUMN FemaleSuicidePercent
+
+UPDATE AgeOverview
+	SET FemaleSuicidePercent=(
+		SELECT round((100*(FemaleNumofSuicide/NumofSuicide)),2)
+		FROM AgeOverview
+		GROUP by Year, AgeGroup)
+		
+ALTER TABLE AgeOverview
+ADD COLUMN FemalePopPercent
+
+UPDATE AgeOverview
+	SET FemalePopPercent=(
+		SELECT round((100*(FemalePopEst/VeteranPopEst)),2)
+		FROM AgeOverview)
+		
+ALTER TABLE AgeOverview
+ADD COLUMN NonRecentVHAMaleSuicidesPercent
+
+UPDATE AgeOverview
+	SET NonRecentVHAMaleSuicidesPercent=(
+		SELECT round((100*(NonRecentVHAsuicides/NumofSuicide)),2)
+		FROM AgeOverview)
+		
+ALTER TABLE AgeOverview add FemaleNumbofSuicide as (NumofSuicide-MaleSuicide)
+
+ALTER TABLE AgeOverview DROP COLUMN FemaleNumofSuicide
+
+ALTER TABLE AgeOverview DROP COLUMN MaleSuicidePercent
+
+ALTER TABLE AgeOverview DROP COLUMN MalePopPercent
+
+ALTER TABLE AgeOverview DROP COLUMN FemaleSuicidePercent
+
+ALTER TABLE AgeOverview DROP COLUMN FemalePopPercent
+
+ALTER TABLE AgeOverview DROP COLUMN NonRecentVHAMaleSuicidesPercent
+
+ALTER TABLE AgeOverview DROP COLUMN FemalePopEst
+
+ALTER TABLE AgeOverview add FemalePopest as (VeteranPopEst-MalePopEst)
+
+ALTER TABLE AgeOverview ADD MaleSuicidePercent as (round((MaleSuicide/NumofSuicide),4)*100)
+
+ALTER TABLE AgeOverview ADD MalePopPercent as (round((MalePopEst/VeteranPopEst),4)*100)
+
+ALTER TABLE AgeOverview ADD FemaleSuicidePercent as (round((FemaleNumbofSuicide/NumofSuicide),4)*100)
+
+ALTER TABLE AgeOverview ADD FemalePopPercent as (round((FemalePopest/VeteranPopEst),4)*100)
+
+ALTER TABLE AgeOverview ADD NonRecentVHAsuicidePercent as (round((NonRecentVHAsuicides/NumofSuicide),4)*100)
+
+ALTER TABLE AgeOverview ADD NonRecentVHApopPercent as (round((NonRecentVHAuserPopEst/VeteranPopEst),4)*100)
+
+ALTER TABLE AgeOverview ADD RecentVHAsuicidePercent as (round((NumOfRecentVHAuserSuicide/VeteranPopEst),4)*100)
+
+ALTER TABLE AgeOverview ADD RecentVHApopPercent as (round((VHAvetPopEst/VeteranPopEst),4)*100)
+
+CREATE TABLE AllOverview as
+	SELECT *
+	FROM AgeOverview
+	WHERE AgeGroup='All'
+	
+ALTER TABLE AllOverview DROP COLUMN AgeGroup
+
+ALTER TABLE YearOverview DROP COLUMN NumofSuicide
+
+ALTER TABLE YearOverview DROP COLUMN NonRecentVHAsuicides
+
+ALTER TABLE YearOverview DROP COLUMN NonRecentVHApop
+
+ALTER TABLE YearOverview DROP COLUMN "NonRecentVHAcrudeRate/100,000"
+
+ALTER TABLE YearOverview DROP COLUMN NumOfRecentVHAuserSuicide
+
+ALTER TABLE YearOverview DROP COLUMN VHAvetPopEst
+
+ALTER TABLE YearOverview DROP COLUMN RecentVHApercentage
+
+ALTER TABLE YearOverview DROP COLUMN SuicideDeaths
+
+ALTER TABLE YearOverview DROP COLUMN VeteranPop
+
+ALTER TABLE YearOverview DROP COLUMN MaleSuicides
+
+ALTER TABLE YearOverview DROP COLUMN MalePopEst
+
+ALTER TABLE YearOverview DROP COLUMN "MaleCrudeRate/100,000"
+
+ALTER TABLE YearOverview DROP COLUMN FemaleSuicides
+
+ALTER TABLE YearOverview DROP COLUMN FemalePopest
+
+ALTER TABLE YearOverview DROP COLUMN "FemaleCrudeRate/100,000"
+
+ALTER TABLE YearOverview DROP COLUMN "RecentVHAuserSuicideCrudeRate/100,000"
+
+ALTER TABLE YearOverview DROP COLUMN NonRecentVHApercentage
+
+ALTER TABLE YearOverview DROP COLUMN "WhiteCrudeRate/100,000"
+
+ALTER TABLE YearOverview DROP COLUMN "AfricanAmericanCrudeRate/100,000"
+
+ALTER TABLE YearOverview DROP COLUMN "NativeCrudeRate/100,000"
+
+ALTER TABLE YearOverview DROP COLUMN "Asian/Hawaiian/PacificIslanderCrudeRate/100,000"
+
+ALTER TABLE YearOverview DROP COLUMN "MultipleRaceCrudeRate"
+
+ALTER TABLE YearOverview DROP COLUMN "CrudeRate/100,000"
+
+ALTER TABLE AllOverview DROP COLUMN "CrudeRate/100,000"
+
+ALTER TABLE AllOverview DROP COLUMN "MaleCrudeRate/100,000"
+
+ALTER TABLE AllOverview DROP COLUMN "FemaleCrudeRate/100,000"
+
+ALTER TABLE AllOverview DROP COLUMN "NonRecentVHAcrudeRate/100,000"
+
+ALTER TABLE AllOverview DROP COLUMN "RecentVHAuserSuicideCrudeRate/100,000"
+
+CREATE TABLE YearMasterSet as
+	SELECT ao.*, yo.*
+	FROM AgeAllOverview ao
+	NATURAL LEFT JOIN YearOverview yo
+
+ALTER TABLE YearMasterSet ADD WhiteNumofSuicidePercent as (round((WhiteNumofSuicide/NumofSuicide),4)*100)
+
+ALTER TABLE YearMasterSet ADD AfricanAmericanNumofSuicidesPercent as (round((AfricanAmericanNumofSuicides/NumofSuicide),4)*100)
+
+ALTER TABLE YearMasterSet ADD NativeNumofSuicidePercent as (round((NativeNumofSuicide/NumofSuicide),4)*100)
+
+ALTER TABLE YearMasterSet ADD "Asian/Hawaiian/PacificIslanderNumofSuicidesPercent" as (round(("Asian/Hawaiian/PacificIslanderNumofSuicides"/NumofSuicide),4)*100)
+
+ALTER TABLE YearMasterSet ADD MultipleRaceNumofSuicidesPercent as (round((MultipleRaceNumofSuicides/NumofSuicide),4)*100)
+
+ALTER TABLE YearMasterSet ADD UnknownRaceSuicidePercent as (round((UnknownRace/NumofSuicide),4)*100)
+
+ALTER TABLE YearMasterSet RENAME COLUMN UnknownRace to UnknownRaceSuicide
+
+ALTER TABLE AgeOverview RENAME to AgeMasterSet
+
+ALTER TABLE YearMasterSet DROP COLUMN RecentVHAsuicidePercent
+
+ALTER TABLE YearMasterSet ADD COLUMN RecentVHAsuicidePercent as (round((NumOfRecentVHAuserSuicide/NumofSuicide),4)*100)
